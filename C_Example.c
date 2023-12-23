ced82263220389a5503612426b603b964b1912e6
Mahmud Hasan <engr.mahmud.hasan.2016@gmail.com>
2023-12-22 21:17:48 -0330

    added feature

--- C_Example.c
+++ C_Example.c
@@ -2,4 +2,12 @@
 int main(void)
 {
   printf("Hello World!");
+  
+  char name[50]; // Assuming the name won't exceed 50 characters
+
+    printf("Please enter your name: ");
+    scanf("%s", name);
+
+    printf("Hello, %s! Nice to meet you.\n", name);
+
 }
